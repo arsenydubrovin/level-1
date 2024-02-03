50b93877d92c7030a0fb1af8a4dff95d758a4288
Arseny Dubrovin <arsenydubrovin@arsenydubrovin.ru>
2024-02-03 18:29:29 +0300

    task 10

---
+++ task_10/main.go
@@ -0,0 +1,28 @@
+// Дана последовательность температурных колебаний: -25.4, -27.0 13.0, 19.0, 15.5, 24.5, -21.0, 32.5. Объединить данные значения в группы с шагом в 10 градусов. Последовательность в подмножноствах не важна.
+
+// Пример: -20:{-25.0, -27.0, -21.0}, 10:{13.0, 19.0, 15.5}, 20: {24.5}, etc.
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	observations := []float64{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
+
+	fmt.Println(groupObservations(observations))
+}
+
+func groupObservations(observations []float64) map[int][]float64 {
+	groups := make(map[int][]float64)
+
+	for _, obs := range observations {
+		// Вычисление индекса для группировки
+		i := int(obs) / 10 * 10
+
+		groups[i] = append(groups[i], obs)
+	}
+
+	return groups
+}
